f5b96dcc2580cec0c09a225556d2957a51d764ec
Влад Бочкарёв <ichi222@yandex.ru>
2022-05-08 20:55:07 +0300

    base.html added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+def group_posts(request, any_slug):
+    template_group_posts = 'posts/group_list.html'
+    return render(request, template_group_posts)
 
 # Create your views here.

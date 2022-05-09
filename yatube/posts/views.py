e2f680c787df05581a251db3bd86efdbd8f7ce5e
Влад Бочкарёв <ichi222@yandex.ru>
2022-05-09 13:57:23 +0300

    titles added

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,12 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    context = { 'title':title }
+    return render(request, template, context)
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
-def group_posts(request, any_slug):
+def group_posts(request, slug):
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = { 'title':title }
     template_group_posts = 'posts/group_list.html'
-    return render(request, template_group_posts)
+    return render(request, template_group_posts, context)
 
 # Create your views here.
